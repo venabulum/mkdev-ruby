f39bfbe40c78ca99ba1530f935a5b6de3b33f24e
venabulum <nullmoon24@gmail.com>
2016-07-12 14:57:04 +0000

    Use csv, ostruct and date module for improve our code

--- general/movies_app.rb
+++ general/movies_app.rb
@@ -1,5 +1,9 @@
+require 'csv'
+require 'ostruct'
+require 'date'
+
 file_name = ARGV.first
-default_name = "movies.txt"
+default_name = 'movies.txt'
 file_name ||= default_name
 
 # проверяем наличие файла и выходим, если он отсутствует
@@ -9,25 +13,29 @@ unless File.exist?(file_name)
 end
 
 # преобразуем строки из нашего файла в словарь фильмов
-KEYS = [:url, :name, :year, :country, :date, :genre, :length, :rating, :director, :artist] 
-films = open(file_name).map { |lines| lines.split('|') }.map { |value| KEYS.zip(value).to_h }
+KEYS = [:url, :name, :year, :country, :date, :genre, :length, :rating, :director, :artist]
+films = CSV.read(file_name, col_sep: '|').map { |value| OpenStruct.new(KEYS.zip(value).to_h) }
 
 # сортируем массив словарей по длине фильма и отбираем 5 самых длинных
-longest_films = films.sort_by { |film| film[:length].to_i }.last(5)
+longest_films = films.sort_by { |film| film.length.to_i }.last(5)
 
 # а теперь 10 самых первых по дате выхода комедий
-oldest_comedy = films.select { |film| film[:genre].include?("Comedy") }.sort_by { |film| film[:date] }.first(10)
+oldest_comedy = films.select { |film| film.genre.include?("Comedy") }.sort_by { |film| film.date }.first(10)
 
 # составляем уникальный алфавитный список всех режиссеров
-directors = films.map { |film| film[:director] }.uniq.sort_by { |full_name| full_name.split(' ').last }
+directors = films.map { |film| film.director }.uniq.sort_by { |full_name| full_name.split(' ').last }
 
-# и считаем, сколько фильмов снято не в США
-not_in_usa = films.reject { |film| film[:country].include?("USA") }.count
+# считаем, сколько фильмов снято не в США
+not_in_usa = films.reject { |film| film.country.include?("USA") }.count
+
+# выводим статистику по месяцам — в каком сколько фильмов снято
+stat = films.map { |film| (film.date) }.each { |date| puts Date._parse(date) }
+# puts stat.inspect
 
 # красиво формируем информацию о фильме
 def beautiful(arr)
-  arr.each { |i| 
-    puts "#{i[:name]} (#{i[:date]}; #{i[:genre]}) / #{i[:length]}"
+  arr.each { |film| 
+    puts "#{film.name} (#{film.date}; #{film.genre}) / #{film.length}"
   }
 end
 
@@ -37,14 +45,16 @@ def delimeter
 end
 
 # выводим итоговый результат
-puts "= = = TOP 5 LONGEST FILMS = = ="
-beautiful(longest_films)
-delimeter
-puts "= = = TOP 10 OLDEST COMEDY FILMS = = ="
-beautiful(oldest_comedy)
-delimeter
-puts "= = = A LIST OF ALL DIRECTORS = = ="
-puts directors
-delimeter
-puts "= = = SHOT NOT IN THE US = = ="
-puts  "#{not_in_usa} films out of 250"
+# puts "= = = TOP 5 LONGEST FILMS = = ="
+# beautiful(longest_films)
+# delimeter
+# puts "= = = TOP 10 OLDEST COMEDY FILMS = = ="
+# beautiful(oldest_comedy)
+# delimeter
+# puts "= = = A LIST OF ALL DIRECTORS = = ="
+# puts directors
+# delimeter
+# puts "= = = SHOT NOT IN THE US = = ="
+# puts  "#{not_in_usa} films out of 250"
+# delimeter
+# puts "= = = MONTHS STAT = = ="
